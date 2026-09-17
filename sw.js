28294df62a7080bafb450f978047505cfc18fa6c
evacuatorkrasnodar-ux <evacuatorkrasnodar@gmail.com>
2026-09-17 04:57:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'sites-900-cache-v2';
+const CACHE_NAME = 'sites-900-cache-v3';
 
 const URLS_TO_CACHE = [
-  '/sites-900/',
   '/sites-900/index.html',
   '/sites-900/style.css',
-  '/sites-900/manifest.json'
+  '/sites-900/manifest.json',
+  '/sites-900/icon-192.png',
+  '/sites-900/icon-512.png'
 ];
 
 // Установка Service Worker
@@ -33,7 +34,17 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      if (response) return response;
+
+      return fetch(event.request).then((networkResponse) => {
+        if (event.request.method === 'GET') {
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+          });
+        }
+
+        return networkResponse;
+      });
     })
   );
 });
